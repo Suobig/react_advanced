0f163b234ad71bc7d50dc1f7cda0301833e06cad
Popov Anton <apopovwork@gmail.com>
2026-04-11 11:12:17 +0300

    practice5: исправления и readme

--- src/4-features/refExamples/FocusTracker.tsx
+++ src/4-features/refExamples/FocusTracker.tsx
@@ -10,15 +10,16 @@ export const FocusTracker = () => {
   }
 
   const handleFocus = () => {
+    const focusCount = focusCounter.current + 1
     /* eslint-disable-next-line react-hooks/refs */
-    console.log("Focus changed:", focusCounter.current)
-    focusCounter.current += 1
+    console.log("Focus changed:", focusCount)
+    focusCounter.current = focusCount
   }
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-      <input ref={inputRef} onFocus={handleFocus} placeholder="первый" />
-      <input onFocus={handleFocus} placeholder="второй" />
+      <input ref={inputRef} onFocusCapture={handleFocus} placeholder="первый" />
+      <input onFocusCapture={handleFocus} placeholder="второй" />
       <Button variant="contained" onClick={handleClick}>
         Фокус на первом
       </Button>
